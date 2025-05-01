e433b20b1bc759a7607dc8c1b8a863412c2f4427
sudesnac <sudesnac@gmail.com>
2025-05-01 17:44:38 +0900

    modify english page

--- lang/en.js
+++ lang/en.js
@@ -1,31 +1,22 @@
 // ---------- Navbar titles
 const enNavbarData = {
-  title: " Your name ",
+  title: "Sudesna Chakraborty",
   Home: "Home",
-  publications: "publications",
-  Research: "Research",
-  Jobs: "Jobs",
+  publications: "Publications",
+  Research: "Research & Funding",
+  Jobs: "Employment",
   Contact: "Contact",
 };
 
 // ---------- Home page data
 const enHomePageData = {
-  name: " Your name ",
-  jobTitle: " Job title ",
+  name: "Sudesna Chakraborty",
+  jobTitle: "Assistant Professor",
   home_title: "About me",
   home_content: `
   <div>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <p> 
     </p>
-    <h2 class='title'> Main works </h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh  </p>
-    <ul>
-      <li> Heuristics</li>
-      <li> Policy planning </li>
-      <li> Flexiblity to details </li>
-    </ul>
   </div>
   `,
 };
@@ -36,81 +27,95 @@ const enPublicationsPageData = {
 
   type_one_items: [
     {
-      title: `Dancing to the beat of climate change: Studying visual frames on TikTok through the left-right divide among U.S. politicians (with ).`,
-      abstract: `Politicians across the political spectrum have taken to TikTok in an attempt to reach young voters by producing original videos. This novel trend has turned TikTok into the new polarizing ring in the political communication battle, deepening the partisan breach around issues such as climate change. However`,
+      title: ``,
+      abstract: ``,
       date: "",
-      link: "http://link.com",
+      link: "",
       github: "",
       download: "",
-      writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
+      writers: [],
     },
     {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `Politicians across the political spectrum have taken to TikTok in an attempt to reach young voters by producing original videos. This novel trend has turned TikTok into the new polarizing ring in the political communication battle, deepening the partisan breach around issues such as climate change. However`,
-      date: "1996",
+      title: ``,
+      abstract: ``,
+      date: "",
       link: "",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla", ""],
+      github: "",
+      download: "",
+      writers: [],
+    },
+    {
+      title: ``,
+      abstract: ``,
+      date: "",
+      link: "",
+      github: "",
+      download: "",
+      writers: [],
+    },
+    {
+      title: ``,
+      abstract: ``,
+      date: "",
+      link: "",
+      github: "",
+      download: "",
+      writers: [],
+    },
+    {
+      title: ``,
+      abstract: ``,
+      date: "",
+      link: "",
+      github: "",
+      download: "",
+      writers: [],
     },
   ],
 
-  type_two_title: "Under Review",
+  type_two_title: "International Conference Presentations",
   type_two_items: [
     {
-      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      title: ``,
       abstract: ``,
-      date: "1996",
+      date: "",
       link: "",
       github: "",
+      download: "",
       writers: [],
     },
     {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
+      title: ``,
+      abstract: ``,
+      date: "",
+      link: "",
+      github: "",
+      download: "",
+      writers: [],
     },
   ],
 
-  type_three_title: "Book Chapters",
+  type_three_title: "PhD Thesis",
   type_three_items: [
     {
-      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      title: ``,
       abstract: ``,
       date: "",
       link: "",
       github: "",
       writers: [],
-    },
-    {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla"],
     },
   ],
 
-  type_four_title: "",
+  type_four_title: "Other",
   type_four_items: [
     {
-      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      title: ``,
       abstract: ``,
       date: "",
       link: "",
       github: "",
       writers: [],
-    },
-    {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla"],
     },
   ],
 };
@@ -120,39 +125,84 @@ const enResearchPageData = {
   title: "Research",
   content: `
     <div class='research_content'>
-        <p> My research goal is to combine the three major sub-fields of the Computational Social Science, including Social Media Data Analysis, Network Analysis, and Agent-based Modeling, to study various social science problems. Below you can find a summary of my past and current projects.</p>
-        <br />
-        <h4 style="font-size: 1.1rem "> Identifying and Characterizing US Domestic Ideological Extremists </h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+        <p> My research .
         <img src='../files/images/research.png' />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra  accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat <br /> imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet <br /> lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+        </p>
+        <br />
+        <h4 style="font-size: 1.1rem "> Awards and Funding </h4>
     </div>
   `,
 };
 
 // ---------- Jobs page data
 const enJobsPageData = {
-  title: "Jobs",
+  title: "Research Work Experience",
   items: [
     {
-      title: `Job title`,
-      company: "Company name",
-      startData: "20 April 2019",
-      endDate: "20 April 2022",
-      location: "Berlin",
+      title: `Assistant Professor`,
+      company: "Aoyama Gakuin University",
+      startData: "1 April 2023",
+      endDate: "",
+      location: "Tokyo, JAPAN",
       abstract:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
-      achievements: ["achievement 1", "achievement 2", "achievement 3"],
+        "",
+      achievements: [],
     },
     {
-      title: `Job title`,
-      company: "Company name",
-      startData: "20 April 2019",
+      title: ``,
+      company: "",
+      startData: "",
       endDate: "",
-      location: "Berlin",
+      location: "",
       abstract:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
-      achievements: ["achievement 1", "achievement 2"],
+        "",
+      achievements: [],
+    },
+  ],
+  title: "Teaching Work Experience",
+  items: [
+    {
+      title: `Assistant Professor`,
+      company: "Aoyama Gakuin University",
+      startData: "1 April 2023",
+      endDate: "",
+      location: "Tokyo, JAPAN",
+      abstract:
+        "",
+      achievements: [],
+    },
+    {
+      title: ``,
+      company: "",
+      startData: "",
+      endDate: "",
+      location: "",
+      abstract:
+        "",
+      achievements: [],
+    },
+  ],
+  title: "Non-Academic Work Experience",
+  items: [
+    {
+      title: `Assistant Professor`,
+      company: "Aoyama Gakuin University",
+      startData: "1 April 2023",
+      endDate: "",
+      location: "Tokyo, JAPAN",
+      abstract:
+        "",
+      achievements: [],
+    },
+    {
+      title: ``,
+      company: "",
+      startData: "",
+      endDate: "",
+      location: "",
+      abstract:
+        "",
+      achievements: [],
     },
   ],
 };
