18997062ed5b3c495d9387f1a36f0d553a057c43
sudesna <sudesna@it.aoyama.ac.jp>
2025-05-02 15:24:49 +0900

    ja cv

--- lang/fa.js
+++ lang/fa.js
@@ -12,13 +12,6 @@ const faHomePageData = {
   jobTitle: "助教",
   home_title: "",
   home_content: ` <p> </p>
-    <h2 class='title'> </h2>
-    <p> </p>
-    <ul>
-      <li>  </li>
-      <li>   </li>
-      <li>   </li>
-    </ul>
   `,
 };
 
@@ -102,27 +95,194 @@ const faResearchPageData = {
 };
 
 const faJobsPageData = {
-  title: " سوابق شغلی ",
-  items: [
+  title: "Curriculum Vitae",
+
+  education: [
     {
-      title: `عنوان شغلی `,
-      company: " نام شرکت ",
-      startData: "11 شهریور 1401 ",
-      endDate: "11 مرداد 1402",
-      location: " برلین ",
-      abstract:
-        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.",
-      achievements: [" دستاورد 1 ", " دستاورد 2 ", " دستاورد 3 "],
+      degree: "PhD in Neuroscience",
+      institution: "Schulich School of Medicine and Dentistry, Western University (Canada)",
+      duration: "Sep 2017 – Aug 2023",
+      thesisTitle: "Multimodal connectivity of the human basal forebrain",
+      supervisors: ["Dr. Ali Khan", "Dr. Taylor Schmitz"],
+      link: "https://ir.lib.uwo.ca/etd/9476/",
     },
     {
-      title: `عنوان شغلی `,
-      company: " نام شرکت ",
-      startData: "11 شهریور 1401 ",
-      endDate: "",
-      location: " برلین ",
-      abstract:
-        " لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. ",
-      achievements: [" دستاورد 1 ", " دستاورد 2 "],
+      degree: "MS in Applied Cognition and Neuroscience",
+      institution: "University of Texas at Dallas (USA)",
+      duration: "Aug 2012 – Dec 2013",
+    },
+    {
+      degree: "BS in Neuroscience (Minor: Chemistry)",
+      institution: "University of Pittsburgh (USA)",
+      duration: "Aug 2005 – Apr 2009",
+    },
+  ],
+
+  recognitions: [
+    {
+      title: "BMI International Graduate Student Scholarship",
+      description: "$15,000 annually for 4 years, University of Western Ontario",
+    },
+    {
+      title: "CAIR Aids for young scientists to present at international conferences",
+      description: "Center for Advanced Information technology Research, Aoyama Gakuin University",
+    },
+  ],
+
+  grants: [
+    {
+      title: "KAKENHI Grant-in-Aid for Young Scientists",
+      description: "Japan Society for the Promotion of Science, ¥4,810,000",
+      duration: "2025 – 2028",
+      link: "https://kaken.nii.ac.jp/en/grant/KAKENHI-PROJECT-25K18575/"
+    },
+  ],
+
+  items: [
+    {
+      title: "Assistant Professor",
+      company: "Aoyama Gakuin University, College of Science and Engineering",
+      startData: "April 2024",
+      endDate: null,
+      location: "Japan",
+      abstract: "",
+      achievements: [],
+    },
+    {
+      title: "Associated Research Staff (Honorary Position)",
+      company: "Robarts Research Institute, Western University",
+      startData: "Sep 2023",
+      endDate: null,
+      location: "Canada",
+      abstract: "",
+      achievements: [],
+    },
+    {
+      title: "PhD Researcher",
+      company: "Western University",
+      startData: "May 2019",
+      endDate: "Aug 2023",
+      location: "Canada",
+      abstract: "Multimodal connectivity of the human basal forebrain",
+      achievements: ["Supervised by Dr. Ali Khan and Dr. Taylor Schmitz"],
+    },
+    {
+      title: "Teaching Assistant",
+      company: "Western University",
+      startData: "Aug 2018",
+      endDate: "Dec 2018",
+      location: "Canada",
+      abstract: "Applied Psychology (Psych 2035)",
+      achievements: ["Office hours, student support, exam review preparation"],
+    },
+    {
+      title: "Teaching Assistant",
+      company: "Western University",
+      startData: "Aug 2017",
+      endDate: "Apr 2018",
+      location: "Canada",
+      abstract: "Introduction to Psychology (Psych 1000)",
+      achievements: ["Office hours, grading, review sessions"],
+    },
+    {
+      title: "PhD Researcher",
+      company: "Western University",
+      startData: "Aug 2017",
+      endDate: "Apr 2019",
+      location: "Canada",
+      abstract: "Hippocampus plasticity in treatment of MDD with ECT",
+      achievements: ["Supervised by Dr. Stefan Kohler and Dr. Ali Khan"],
+    },
+    {
+      title: "Senior Consultant",
+      company: "NTT DATA Institute of Management Consulting, Inc.",
+      startData: "Apr 2016",
+      endDate: "Jun 2017",
+      location: "Japan",
+      abstract: "Advised on neuroscience-informed product development",
+      achievements: [],
+    },
+    {
+      title: "Researcher",
+      company: "Araya Brain Imaging Inc.",
+      startData: "Aug 2015",
+      endDate: "Mar 2016",
+      location: "Japan",
+      abstract: "MRI and DTI processing using MATLAB, FSL, FreeSurfer, SPM12",
+      achievements: [],
+    },
+    {
+      title: "Research Assistant",
+      company: "Northwestern University",
+      startData: "Jan 2015",
+      endDate: "Jul 2015",
+      location: "USA",
+      abstract: "Structural MRI for PPA and SuperAging (FreeSurfer, SPM8)",
+      achievements: [],
+    },
+    {
+      title: "Research Assistant",
+      company: "University of Texas at Dallas",
+      startData: "Aug 2013",
+      endDate: "Dec 2014",
+      location: "USA",
+      abstract: "DTI and structural MRI for aging studies",
+      achievements: [],
+    },
+    {
+      title: "Research Assistant",
+      company: "Chiba University",
+      startData: "Jun 2011",
+      endDate: "Jul 2012",
+      location: "Japan",
+      abstract: "fMRI studies on memory, anxiety, and mental rotation",
+      achievements: [],
+    },
+    {
+      title: "Research Assistant",
+      company: "National Institute of Radiological Science",
+      startData: "Dec 2010",
+      endDate: "Mar 2011",
+      location: "Japan",
+      abstract: "7T MRI operations for animal models",
+      achievements: [],
+    },
+    {
+      title: "Research Assistant",
+      company: "University of Pittsburgh",
+      startData: "Sep 2008",
+      endDate: "May 2010",
+      location: "USA",
+      abstract: "fMRI and behavioral studies on working memory and reading",
+      achievements: [],
+    },
+    {
+      title: "Volunteer Translator",
+      company: "IEEE Japan Council Women in Engineering",
+      startData: "May 2009",
+      endDate: "May 2011",
+      location: "Japan",
+      abstract: "Translated English to Japanese for technical and non-technical documents",
+      achievements: [],
+    },
+    {
+      title: "Neurology Intern",
+      company: "Iwate Medical University",
+      startData: "May 2008",
+      endDate: "Aug 2008",
+      location: "Japan",
+      abstract: "MRI and psychiatric evaluations for Alzheimer’s and Parkinson’s patients",
+      achievements: [],
+    },
+  
+    {
+      title: "Library Assistant",
+      company: "University of Pittsburgh, East Asian Library",
+      startData: "Jun 2007",
+      endDate: "Apr 2008",
+      location: "USA",
+      abstract: "Catalogued Japanese books and magazines",
+      achievements: [],
     },
   ],
 };
